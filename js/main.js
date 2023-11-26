1fecacfca9c7d17cc9e62a0e8ff41e04bb132517
pss-aileen <pss.aiando@gmail.com>
2023-11-26 22:52:11 +0800

    add: background function #1

--- js/main.js
+++ js/main.js
@@ -1,7 +1,23 @@
+let color;
+const emoji = "🐶";
+
+function getBackgroundColor() {
+  const input = document.getElementById("background-monochrome");
+  color = input.value;
+  
+  input.addEventListener("input", () => {
+    const target = document.getElementById("background-monochrome");
+    color = target.value;
+    console.log(color);
+    draw();
+  });
+}
+
+getBackgroundColor();
+
 
 
 function draw() {
-
 
   let canvas = document.getElementById("tutorial");
   
@@ -15,19 +31,19 @@ function draw() {
     const x = canvas.width / 2;
     const y = canvas.height / 2;
 
-    context.fillStyle = "skyblue";
+    context.fillStyle = color;
     context.fillRect(0, 0, width, height);
 
     context.font = "normal 140px/1 serif";
     context.textAlign = "center";
     context.textBaseline = "middle";
-    context.shadowColor = "rgba(0, 0, 0, 0.2)"; // 影の色
+    // context.shadowColor = "rgba(0, 0, 0, 0.2)"; // 影の色
     context.shadowBlur = 16; // 影のぼかしの強さ
     context.shadowOffsetX = 0; // 影のオフセット（水平方向）
     context.shadowOffsetY = 4; // 影のオフセット（垂直方向）
     
-    const emojiHeight = context.measureText("😍").actualBoundingBoxAscent;
-    context.strokeText("😍", x, y);
+    const emojiHeight = context.measureText(emoji).actualBoundingBoxAscent;
+    context.strokeText(emoji, x, y);
   } else {
     console.log("no context");
   }
