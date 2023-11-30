473eb85b06d54e5cb54c6ffc6f1dfd6c2e087702
pss-aileen <140990100+pss-aileen@users.noreply.github.com>
2023-11-30 17:06:11 +0800

    Refactor: done draw functions before rere refactor

--- js/main.js
+++ js/main.js
@@ -1,182 +1,122 @@
-// 単色モードかグラデーションモードかを選ぶ
-// 単色を選んだら、単色のカラーピ取得、描画
-// グラデーションを選んだら、グラデの色を取得、描画
+"use strict";
+{
+  // 初期描画
+  drawBase();
+  drawMono();
+  drawIcon();
 
-// canvasをdrawする関数
-// アイコン描画
-// drawIcon();
-// そのあとに背景描画？
-// drawGradientColor()
-// drawMonoColor()
-  
-// 単色を選ぶたび、グラデを選ぶたびにアップデートされるようにする
-// 色をそれぞれ選ぶごとに描画されるようにする
+  // イベントトリガー
+  const selectInput = document.getElementById("color-type");
+  selectInput.addEventListener("change", (e) => {
+    const mode = selectMode();
+    setMode(mode);
+  });
 
-drawBase();
+  const monoInput = document.getElementById("background-monochrome");
+  monoInput.addEventListener("input", () => {
+    drawMono();
+    drawIcon();
+  });
 
-function drawBase() {
-  const canvas = document.getElementById("canvas-body");
-  const context = canvas.getContext("2d");
+  const gradientStartInput = document.getElementById(
+    "background-gradient-start"
+  );
+  const gradientEndInput = document.getElementById("background-gradient-end");
+  gradientStartInput.addEventListener("input", () => {
+    drawGradient();
+    drawIcon();
+  });
+  gradientEndInput.addEventListener("input", () => {
+    drawGradient();
+    drawIcon();
+  });
 
-  const width = canvas.width;
-  const height = canvas.height;
-  context.fillStyle = "pink";
-  context.fillRect(0, 0, width, height);
+  const btn = document.getElementById("btn");
+  btn.addEventListener("click", downloadCanvas);
 
-  return { canvas, context, width, height };
+  // 関数
+
+  function selectMode() {
+    const select = document.getElementById("color-type");
+    return select.value;
+  }
+
+  function setMode(mode) {
+    if (mode === "monochrome") {
+      drawMono();
+      drawIcon();
+    } else if (mode === "gradient") {
+      drawGradient();
+      drawIcon();
+    }
+  }
+
+  function drawBase() {
+    const canvas = document.getElementById("canvas-body");
+    const context = canvas.getContext("2d");
+
+    const width = canvas.width;
+    const height = canvas.height;
+
+    return { canvas, context, width, height };
+  }
+
+  function drawIcon() {
+    const { canvas, context, width, height } = drawBase();
+
+    const imagePath = "images/icon.svg";
+    const image = new Image();
+    image.src = imagePath;
+    image.onload = function () {
+      context.drawImage(image, 0, 0, canvas.width, canvas.height);
+    };
+  }
+
+  function drawGradient() {
+    const { canvas, context, width, height } = drawBase();
+
+    const startColorInput = document.getElementById(
+      "background-gradient-start"
+    );
+    const endColorInput = document.getElementById("background-gradient-end");
+
+    const startColor = startColorInput.value;
+    const endColor = endColorInput.value;
+
+    console.log(startColor);
+    console.log(endColor);
+
+    const gradient = context.createLinearGradient(0, 0, width, height);
+
+    gradient.addColorStop(0, startColor);
+    gradient.addColorStop(1, endColor);
+
+    context.fillStyle = gradient;
+    context.fillRect(0, 0, width, height);
+  }
+
+  function drawMono() {
+    const { canvas, context, width, height } = drawBase();
+
+    const colorInput = document.getElementById("background-monochrome");
+    const color = colorInput.value;
+
+    context.fillStyle = color;
+    context.fillRect(0, 0, width, height);
+    console.log(color);
+  }
+
+  function downloadCanvas() {
+    const canvas = document.getElementById("canvas-body");
+
+    const dataURL = canvas.toDataURL("image/png");
+    const downloadLink = document.createElement("a");
+    downloadLink.href = dataURL;
+
+    downloadLink.download = "canvas_image.png";
+
+    document.body.appendChild(downloadLink);
+    downloadLink.click();
+    document.body.removeChild(downloadLink);
+  }
 }
-
-drawIcon();
-
-function drawIcon() {
-  const { canvas, context, width, height } = drawBase();
-
-  const imagePath = 'images/icon.png';
-  const image = new Image();
-  image.src = imagePath;
-  image.onload = function() {
-    context.drawImage(image, 0, 0, canvas.width, canvas.height);
-  };
-}
-
-drawGradient();
-
-function drawGradient() {
-  const { canvas, context, width, height } = drawBase();
-
-  let startColor = document.getElementById("background-gradient-start");
-  let endColor = document.getElementById("background-gradient-end");
-
-  startColor = startColor.value;
-  endColor = endColor.value;
-
-  console.log(startColor);
-  console.log(endColor);
-
-  const gradient = context.createLinearGradient(0, 0, width , height);
-
-  gradient.addColorStop(0, startColor);
-  gradient.addColorStop(1, endColor);
-
-  context.fillStyle = gradient;
-  context.fillRect(0, 0, width, height);
-}
-
-
-
-// let color;
-
-// function getBackgroundColor() {
-//   const input = document.getElementById("background-monochrome");
-//   color = input.value;
-  
-//   input.addEventListener("input", () => {
-//     const target = document.getElementById("background-monochrome");
-//     color = target.value;
-//     console.log(color);
-//     draw();
-//   });
-// }
-
-// getBackgroundColor();
-
-
-// const select = document.getElementById("color-type");
-
-// select.addEventListener("change", () => {
-//   const target = document.getElementById("color-type");
-//   console.log(target.value);
-// });
-
-
-// const gradientStart = document.getElementById("background-gradient-start");
-// const gradientEnd = document.getElementById("background-gradient-end");
-
-// console.log(gradientStart.value);
-// console.log(gradientEnd.value);
-
-// getGradientProperty();
-// setGradient();
-
-// function getGradientProperty() {
-//   const gradientStart = document.getElementById("background-gradient-start");
-//   const gradientEnd = document.getElementById("background-gradient-end");
-  
-//   const gradientStartColor = gradientStart.value;
-//   const gradientEndColor = gradientEnd.value;
-  
-//   return { gradientStartColor, gradientEndColor };
-// }
-
-// function setGradient(start, end) {
-//   const testArea = document.getElementById("test-content");
-//   testArea.style.backgroundImage = `linear-gradient(0deg, ${gradientEnd.value}, ${gradientStart.value})`;
-// }
-
-// gradientStart.addEventListener("change", () => {
-//   const { gradientStartColor, gradientEndColor } = getGradientProperty();
-//   setGradient(gradientStartColor, gradientEndColor);
-//   draw(gradientStartColor, gradientEndColor);
-// });
-
-// gradientEnd.addEventListener("change", () => {
-//   const { gradientStartColor, gradientEndColor } = getGradientProperty();
-//   setGradient(gradientStartColor, gradientEndColor);
-//   draw(gradientStartColor, gradientEndColor);
-// });
-
-
-
-// function draw(gradientStart, gradientEnd) {
-
-//   let canvas = document.getElementById("tutorial");
-  
-//   if (canvas.getContext) {
-//     let context = canvas.getContext("2d");
-//     console.log("there is context");
-
-//     const width = canvas.width;
-//     const height = canvas.height;
-
-//     const x = canvas.width / 2;
-//     const y = canvas.height / 2;
-
-//     const gradient = context.createLinearGradient(0, 0, width , height);
-
-//     gradient.addColorStop(0, `${gradientStart}`);
-//     gradient.addColorStop(1, `${gradientEnd}`);
-
-//     const imagePath = '../images/icon.png';
-//     const image = new Image();
-//     image.onload = function() {
-//       context.drawImage(image, 0, 0, canvas.width, canvas.height);
-//     };
-//     image.src = imagePath;
-
-//     context.fillStyle = gradient;
-//     context.fillRect(0, 0, width, height);
-//   } else {
-//     console.log("no context");
-//   }
-// }
-
-// draw("red", "yellow");
-
-
-// function downloadCanvas() {
-//   let canvas = document.getElementById("tutorial");
-
-//   let dataURL = canvas.toDataURL("image/png");
-//   let downloadLink = document.createElement("a");
-//   downloadLink.href = dataURL;
-  
-//   downloadLink.download = "canvas_image.png";
-
-//   document.body.appendChild(downloadLink);
-//   downloadLink.click();
-//   document.body.removeChild(downloadLink);
-// }
-
-// const btn = document.getElementById("btn");
-
-// btn.addEventListener("click", downloadCanvas);
